bf52d3f2c59e03d170b09ff89554a5e41272ec41
Saddam-tech <salokhiddinov0727@gmail.com>
2022-01-11 16:22:06 +0900

    dist generated

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,56 @@
 <template>
   <div class="container">
     <h1>FCM push notifications</h1>
-    <!-- <a @click="authenticate">Authenticate</a> -->
+    <a @click="authenticate">Authenticate</a>
   </div>
 </template>
 
 <script>
-// import { getAuth, signInAnonymously } from "firebase/auth";
-// import { getMessaging, onMessage, getToken } from "firebase/messaging";
-// import { messaging } from "@/plugins/firebase";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getMessaging, onMessage, getToken } from "firebase/messaging";
+import { messaging } from "@/plugins/firebase";
 
 export default {
   name: "IndexPage",
-  // mounted() {
-  //   function isWindowControllerSupported() {
-  //     return (
-  //       navigator.cookieEnabled &&
-  //       "serviceWorker" in navigator &&
-  //       "PushManager" in window &&
-  //       "Notification" in window &&
-  //       "fetch" in window &&
-  //       ServiceWorkerRegistration.prototype.hasOwnProperty(
-  //         "showNotification"
-  //       ) &&
-  //       PushSubscription.prototype.hasOwnProperty("getKey")
-  //     );
-  //   }
+  mounted() {
+    function isWindowControllerSupported() {
+      return (
+        navigator.cookieEnabled &&
+        "serviceWorker" in navigator &&
+        "PushManager" in window &&
+        "Notification" in window &&
+        "fetch" in window &&
+        ServiceWorkerRegistration.prototype.hasOwnProperty(
+          "showNotification"
+        ) &&
+        PushSubscription.prototype.hasOwnProperty("getKey")
+      );
+    }
 
-  //   isWindowControllerSupported();
-  //   const messaging = getMessaging();
+    isWindowControllerSupported();
+    const messaging = getMessaging();
 
-  //   onMessage(messaging, (payload) => {
-  //     console.log("Message on client: ", payload);
-  //   });
-  // },
-  // methods: {
-  //   async authenticate() {
-  //     await signInAnonymously(getAuth());
-  //     this.activate();
-  //   },
-  //   async activate() {
-  //     const token = await getToken(messaging, {
-  //       vapidKey:
-  //         "BHVpfN_OJcA9743IK5yHv4mTuOHKPE8Gh_y62i_SUcKt8Ue1CGhHX4JEOsMCMwAfrfMMZtzZ98p19Hpq-1XDHlQ",
-  //     });
+    onMessage(messaging, (payload) => {
+      console.log("Message on client: ", payload);
+    });
+  },
+  methods: {
+    async authenticate() {
+      await signInAnonymously(getAuth());
+      this.activate();
+    },
+    async activate() {
+      const token = await getToken(messaging, {
+        vapidKey:
+          "BHVpfN_OJcA9743IK5yHv4mTuOHKPE8Gh_y62i_SUcKt8Ue1CGhHX4JEOsMCMwAfrfMMZtzZ98p19Hpq-1XDHlQ",
+      });
 
-  //     if (token) {
-  //       console.log(token);
-  //     } else {
-  //       // Request permission
-  //     }
-  //   },
-  // },
+      if (token) {
+        console.log(token);
+      } else {
+        // Request permission
+      }
+    },
+  },
 };
 </script>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,18 +1,18 @@
-// import { initializeApp, getApps } from "firebase/app";
-// import { getMessaging } from "firebase/messaging";
+import { initializeApp, getApps } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD3bct9uVtLgqmDMv2oJB4xRQvZOpGiLaY",
-//   authDomain: "dmpush-fcm.firebaseapp.com",
-//   projectId: "dmpush-fcm",
-//   storageBucket: "dmpush-fcm.appspot.com",
-//   messagingSenderId: "264398501643",
-//   appId: "1:264398501643:web:8619dd4978d6abb7f9940e",
-//   measurementId: "G-Y32GK16D1G",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyD3bct9uVtLgqmDMv2oJB4xRQvZOpGiLaY",
+  authDomain: "dmpush-fcm.firebaseapp.com",
+  projectId: "dmpush-fcm",
+  storageBucket: "dmpush-fcm.appspot.com",
+  messagingSenderId: "264398501643",
+  appId: "1:264398501643:web:8619dd4978d6abb7f9940e",
+  measurementId: "G-Y32GK16D1G",
+};
 
-// const apps = getApps();
+const apps = getApps();
 
-// const app = !apps.length ? initializeApp(firebaseConfig) : apps[0];
+const app = !apps.length ? initializeApp(firebaseConfig) : apps[0];
 
-// export const messaging = getMessaging(app);
+export const messaging = getMessaging(app);
