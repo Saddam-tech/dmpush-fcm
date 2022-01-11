a182f9929a7fde2babeb3c3d709d6c35edfec725
Saddam-tech <salokhiddinov0727@gmail.com>
2022-01-11 14:26:55 +0900

    first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3bct9uVtLgqmDMv2oJB4xRQvZOpGiLaY",
+  authDomain: "dmpush-fcm.firebaseapp.com",
+  projectId: "dmpush-fcm",
+  storageBucket: "dmpush-fcm.appspot.com",
+  messagingSenderId: "264398501643",
+  appId: "1:264398501643:web:8619dd4978d6abb7f9940e",
+  measurementId: "G-Y32GK16D1G",
+};
+
+const apps = getApps();
+
+const app = !apps.length ? initializeApp(firebaseConfig) : apps[0];
+
+export const messaging = getMessaging(app);
---
+++ static/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3bct9uVtLgqmDMv2oJB4xRQvZOpGiLaY",
+  authDomain: "dmpush-fcm.firebaseapp.com",
+  projectId: "dmpush-fcm",
+  storageBucket: "dmpush-fcm.appspot.com",
+  messagingSenderId: "264398501643",
+  appId: "1:264398501643:web:8619dd4978d6abb7f9940e",
+  measurementId: "G-Y32GK16D1G",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("[Firebase message]:", payload);
+});
