c174833df44482d72fbeb13de2893c7302a6ac8d
Saddam-tech <salokhiddinov0727@gmail.com>
2022-01-11 16:02:29 +0900

    latest

---
+++ .nuxt/dist/server/pages/index.js
@@ -0,0 +1,98 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ 23:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=f540028c&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"container"},[_vm._ssrNode("<h1>FCM push notifications</h1>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=f540028c&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+// import { getAuth, signInAnonymously } from "firebase/auth";
+// import { getMessaging, onMessage, getToken } from "firebase/messaging";
+// import { messaging } from "@/plugins/firebase";
+/* harmony default export */ var lib_vue_loader_options_pagesvue_type_script_lang_js_ = ({
+  name: "IndexPage" // mounted() {
+  //   function isWindowControllerSupported() {
+  //     return (
+  //       navigator.cookieEnabled &&
+  //       "serviceWorker" in navigator &&
+  //       "PushManager" in window &&
+  //       "Notification" in window &&
+  //       "fetch" in window &&
+  //       ServiceWorkerRegistration.prototype.hasOwnProperty(
+  //         "showNotification"
+  //       ) &&
+  //       PushSubscription.prototype.hasOwnProperty("getKey")
+  //     );
+  //   }
+  //   isWindowControllerSupported();
+  //   const messaging = getMessaging();
+  //   onMessage(messaging, (payload) => {
+  //     console.log("Message on client: ", payload);
+  //   });
+  // },
+  // methods: {
+  //   async authenticate() {
+  //     await signInAnonymously(getAuth());
+  //     this.activate();
+  //   },
+  //   async activate() {
+  //     const token = await getToken(messaging, {
+  //       vapidKey:
+  //         "BHVpfN_OJcA9743IK5yHv4mTuOHKPE8Gh_y62i_SUcKt8Ue1CGhHX4JEOsMCMwAfrfMMZtzZ98p19Hpq-1XDHlQ",
+  //     });
+  //     if (token) {
+  //       console.log(token);
+  //     } else {
+  //       // Request permission
+  //     }
+  //   },
+  // },
+
+});
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pagesvue_type_script_lang_js_ = (lib_vue_loader_options_pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "62da34d9"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
